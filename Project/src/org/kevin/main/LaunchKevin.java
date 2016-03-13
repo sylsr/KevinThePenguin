0811536e2b9c1b1084907186c999864e141e788c
Tyler Manning <sylsr9@gmail.com>
2016-03-12 18:50:15 -0700

    Framework and build script

--- Project/src/org/kevin/main/LaunchKevin.java
+++ Project/src/org/kevin/main/LaunchKevin.java
@@ -1,5 +1,16 @@
 package org.kevin.main;
 
-public class LaunchKevin {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class LaunchKevin {
+	public static void main(String[]args){
+		JFrame frame = new JFrame();
+		frame.setPreferredSize(new Dimension(500,500));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
--- Project/src/org/kevin/worldbuilder/Creator.java
+++ Project/src/org/kevin/worldbuilder/Creator.java
@@ -1,5 +1,10 @@
 package org.kevin.worldbuilder;
 
+/**
+ * This code is used for creating a level, it reads xml files in and parses them into game objects.
+ * @author Tyler Manning
+ *
+ */
 public class Creator {
 
 }
