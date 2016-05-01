1cee0a2d0989460c72d92bb7ef89545fb2076109
Tyler Manning <sylsr9@gmail.com>
2016-04-30 19:14:45 -0600

    Fixed project to include dependencies during build

--- Project/build/build.sh
+++ Project/build/build.sh
@@ -3,17 +3,31 @@
 BUILD_DIR=$(PWD)
 cd ../
 BASEDIR=$(PWD)
+cd lib
+LIB=$(PWD)
 
 compile(){
 echo Createing directory $BUILD_DIR/output
 mkdir $BUILD_DIR/output
 #Compile all java sources
 cd $BASEDIR
+echo Adding dependencies to the classpath
+
+
+for i in $LIB/*.jar; do
+    CLASSPATH=$CLASSPATH:$i
+done
+CLASSPATH=`echo $CLASSPATH | cut -c2-`
+
+echo $CLASSPATH
+
 echo Moving into $BASEDIR for compile
 rm -rf $BUILD_DIR/sources.txt
 cmd.exe /C "dir /s /B "*.java" > build\sources.txt"
+
 echo Generated $BUILD_DIR/sources.txt
-javac -verbose @$BUILD_DIR/sources.txt -d $BASEDIR/bin
+javac -verbose -classpath $CLASSPATH @$BUILD_DIR/sources.txt -d $BASEDIR/bin
+
 echo Creating jar file in $BUILD_DIR/output
 cd $BASEDIR
 cd bin
